30f6e565e2968c639574c779bf52e6822804f6dd
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-08-04 03:09:08 +0000

    terminado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Template for Machine Learning projects
+# NLP Project
+
+Implementing Natural Language Processing techniques and the Support Vector Machine (SVM) algorithm for classification, we are going to build a spam detector, this time using URLs (https://raw.githubusercontent.com/4GeeksAcademy/NLP-project-tutorial/main/url_spam.csv).
--- src/app.py
+++ src/app.py
@@ -1,4 +1,107 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import pickle
+import re
+import unicodedata
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
-# your code here
+df_raw = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/NLP-project-tutorial/main/url_spam.csv')
+
+def comas(text):
+    """
+    Elimina comas del texto
+    """
+    return re.sub(',', ' ', text)
+
+def espacios(text):
+    """
+    Elimina enters dobles por un solo enter
+    """
+    return re.sub(r'(\n{2,})','\n', text)
+
+def minuscula(text):
+    """
+    Cambia mayusculas a minusculas
+    """
+    return text.lower()
+
+def numeros(text):
+    """
+    Sustituye los numeros
+    """
+    return re.sub('([\d]+)', ' ', text)
+
+def caracteres_no_alfanumericos(text):
+    """
+    Sustituye caracteres raros, no digitos y letras
+    Ej. hola 'pepito' como le va? -> hola pepito como le va
+    """
+    return re.sub("(\\W)+"," ",text)
+
+def comillas(text):
+    """
+    Sustituye comillas por un espacio
+    Ej. hola 'pepito' como le va? -> hola pepito como le va?
+    """
+    return re.sub("'"," ", text)
+
+def palabras_repetidas(text):
+    """
+    Sustituye palabras repetidas
+
+    Ej. hola hola, como les va? a a ustedes -> hola, como les va? a ustedes
+    """
+    return re.sub(r'\b(\w+)( \1\b)+', r'\1', text)
+
+def esp_multiple(text):
+    """
+    Sustituye los espacios dobles entre palabras
+    """
+    return re.sub(' +', ' ',text)
+
+
+
+def url(text):
+    return re.sub(r'(https://www|https://)', '', text)
+
+
+
+# dataset cleaning
+df = df_raw.copy()
+
+df = df.drop_duplicates().reset_index(drop = True)
+df['url_limpia'] = df['url'].apply(url).apply(caracteres_no_alfanumericos).apply(esp_multiple)
+df['is_spam'] = df['is_spam'].apply(lambda x: 1 if x == True else 0)
+
+
+#data processing
+
+vec = CountVectorizer().fit_transform(df['url_limpia'])
+X_train, X_test, y_train, y_test = train_test_split(vec, df['is_spam'], stratify = df['is_spam'], random_state = 2207)
+
+
+#SVM
+
+classifier = SVC(C = 1.0, kernel = 'linear', gamma = 'auto')
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+
+# optimizing hyerparameters
+
+param_grid = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+
+grid = GridSearchCV(SVC(random_state=1234),param_grid,verbose=2)
+grid.fit(X_train,y_train)
+
+best_model = grid
+
+
+filename = '/workspace/NLP-Project/models/best_model.sav'
+pickle.dump(best_model, open(filename,'wb'))
+
+
+df.to_csv('/workspace/NLP-Project/data/processed/df_processed.csv', index = False, encoding='utf-8')
